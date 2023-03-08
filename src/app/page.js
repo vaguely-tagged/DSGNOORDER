d100047168b6623149c0fface07b573de0daae1a
Vincent Sgherzi <sgherzivincent@gmail.com>
2023-03-08 12:45:47 -0800

    init menu layout just to get some code down for rn

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,35 @@
-import { Inter } from 'next/font/google'
+//styles
+let gridStyle = {
+  display: "grid",
+  gridTemplateColumns: "1fr 1fr",
+  justifyContent: "center"
+}
 
-const inter = Inter({ subsets: ['latin'] })
+let itemMarginStyle = {
+  marginBottom: "30px"
+}
+
+let imageSize = {
+  width: "90%",
+  height: "100%",
+  backgroundColor: "black"
+}
 
 export default function Home() {
   return (
-    <div>test</div>
+    <div>
+
+      <div style={gridStyle}>
+        <div>
+          <button style={itemMarginStyle} className="button is-large is-fullwidth is-rounded is-link">Menu</button>
+          <button style={itemMarginStyle} className="button is-large is-fullwidth is-rounded is-link">Preferences</button>
+          <button style={itemMarginStyle} className="button is-large is-fullwidth is-rounded is-link">Search</button>
+          <button className="button is-large is-fullwidth is-rounded is-link">Fav's</button>
+        </div>
+        <div style={imageSize}>hello</div>
+      </div>
+
+    </div>
+
   )
 }
